397d84ecae6a127504a52a650f5ba0357069fda4
Bill Luu <billluu619@gmail.com>
2020-11-06 11:55:11 -0700

    Player & NPC prototype

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    // private Animator anim;
+    private NavMeshAgent agent;
+    
+    void Awake()
+    {
+        // amin = GetComponentAnimator();
+        agent = GetComponent<NavMeshAgent>();
+    }
+}
